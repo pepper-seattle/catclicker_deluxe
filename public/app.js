ae27f364ed445be76d47baa4cbb138633afed81e
Jason Kaczmarsky <jkaczmarsky@gmail.com>
2018-10-10 22:49:58 -0700

    Revert "Show a random cat after every click"
    This reverts commit bc77443870891cfe49d45602c4104bfd8762afa3.

--- public/app.js
+++ public/app.js
@@ -6,106 +6,71 @@ let width = window.innerWidth;
 let height = window.innerHeight;
 let factor;
 let responsiveSize;
-let kittyPool = [];
-let kittyPoolLimit = 10;
-
-/**
- * Preloads some cat images to be used within the game
- */
-(function preloadCats() {
-  kittyPool.push((new Image().src = 'cat.png'));
-
-  function getRandomSize() {
-    let minSize = 200;
-    let maxSize = 600;
-
-    return [
-      minSize + Math.floor(Math.random() * (maxSize - minSize)),
-      minSize + Math.floor(Math.random() * (maxSize - minSize))
-    ];
-  }
-
-  while (kittyPool.length < kittyPoolLimit) {
-    let size = getRandomSize();
-    kittyPool.push(
-      (new Image().src = `https://placekitten.com/${size[0]}/${size[1]}`)
-    );
-  }
-})();
-
-/**
- * Gets a random cat image from the pool
- */
-function getCatImage() {
-  return kittyPool[Math.floor(Math.random() * kittyPoolLimit)];
-}
 
 // changes the size factor of the cat depending on the screen size
 function dietPlan() {
-  if (width <= 750) {
-    factor = 0.3;
-  } else {
-    factor = 0.2;
-  }
-  responsiveSize = width * factor;
-  cat.style.width = `${responsiveSize}px`;
-  cat.style.height = `${responsiveSize}px`;
-}
+	if (width <= 750) {
+		factor = .3;
+	} else {
+		factor = .2;
+	}
+	responsiveSize = width * factor;
+	cat.style.width = `${responsiveSize}px`;
+	cat.style.height = `${responsiveSize}px`;
+};
 
 // moves cat around to a random part of the screen
 function catJump() {
-  let randomWidth = Math.floor(Math.random() * (width - responsiveSize) + 1);
-  let randomHeight = Math.floor(Math.random() * (height - responsiveSize) + 1);
-  let header = document.querySelector('header');
-  let headerHeight = header.offsetHeight;
-  if (randomHeight <= headerHeight) {
-    randomHeight += headerHeight;
-  }
-  cat.style.left = `${randomWidth}px`;
-  cat.style.top = `${randomHeight}px`;
-}
+	let randomWidth = Math.floor((Math.random() * (width - responsiveSize)) + 1);
+	let randomHeight = Math.floor((Math.random() * (height - responsiveSize)) + 1);
+	let header = document.querySelector("header");
+	let headerHeight = header.offsetHeight;
+	if (randomHeight <= headerHeight) {
+		randomHeight += headerHeight;
+	};
+	cat.style.left = `${randomWidth}px`;
+	cat.style.top = `${randomHeight}px`;
+};
 
 // sets initial size and position of cat
 dietPlan();
 catJump();
 
 // adds to counter and randomly moves cat upon click
-const counter = document.querySelector('.counter');
+const counter = document.querySelector(".counter");
 cat.onclick = () => {
   meow.play();
   count += 1;
-  counter.innerHTML = 'Clicks: ' + count;
-  cat.src = getCatImage();
-  catJump();
+	counter.innerHTML = "Clicks: " + count;
+	catJump();
 };
 
 // resets counter upon click
 counter.onclick = () => {
-  count = 0;
-  counter.innerHTML = 'Clicks: ' + count;
+	count = 0;
+  counter.innerHTML = "Clicks: " + count;
 };
 
 // resizes and moves cat upon window resize
-window.addEventListener('resize', () => {
-  width = window.innerWidth;
-  height = window.innerHeight;
-  dietPlan();
-  catJump();
+window.addEventListener("resize", () => {
+	width = window.innerWidth;
+	height = window.innerHeight;
+	dietPlan();
+	catJump();
 });
 
 //fetch API for cat's fact upon button click
 document.getElementById('catButton').onclick = () => {
-  var proxyUrl = 'https://cors-anywhere.herokuapp.com/',
-    targetUrl = 'https://catfact.ninja/fact?max_length=200';
-
-  fetch(proxyUrl + targetUrl)
-    .then(response => response.json())
-    .then(data => {
-      document.getElementById('catFact').innerHTML = data.fact;
-      return data;
-    })
-    .catch(e => {
-      console.log(e);
-      return e;
-    });
+var proxyUrl = 'https://cors-anywhere.herokuapp.com/',
+    targetUrl = 'https://catfact.ninja/fact?max_length=200'
+fetch(proxyUrl + targetUrl)
+  .then(response => response.json())
+  .then(data => {
+    document.getElementById('catFact').innerHTML = data.fact;
+    return data;
+  })
+  .catch(e => {
+    console.log(e);
+    return e;
+  });
 };
