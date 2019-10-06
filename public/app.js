b1d5b3147e427f93d6ab68619e0a58638d3248af
coyote-dev <53406725+coyote-dev@users.noreply.github.com>
2019-10-06 09:54:11 +0300

    Refactor foor loops

--- public/app.js
+++ public/app.js
@@ -15,14 +15,14 @@ function dietPlan() {
 		factor = .2;
 	}
 	responsiveSize = width * factor;
-	for(let i = 0, len = cats.length; i < len; i++) {
-		cats[i].style.width = `${responsiveSize}px`;
-		cats[i].style.height = `${responsiveSize}px`;
+	for(const cat of cats) {
+		cat.style.width = `${responsiveSize}px`;
+		cat.style.height = `${responsiveSize}px`;
 	}
 };
 
 // moves cat around to a random part of the screen
-function catJump(catIndex) {
+function catJump(cat) {
 	let randomWidth = Math.floor((Math.random() * (width - responsiveSize)) + 1);
 	let randomHeight = Math.floor((Math.random() * (height - responsiveSize)) + 1);
 	let header = document.querySelector("header");
@@ -30,31 +30,31 @@ function catJump(catIndex) {
 	if (randomHeight <= headerHeight) {
 		randomHeight += headerHeight;
 	};
-	cats[catIndex].style.left = `${randomWidth}px`;
-	cats[catIndex].style.top = `${randomHeight}px`;
+	cat.style.left = `${randomWidth}px`;
+	cat.style.top = `${randomHeight}px`;
 };
 
 // sets initial size and position of cat
 dietPlan();
-for(let i = 0, len = cats.length; i < len; i++) {
-	catJump(i);
+for(const cat of cats) {
+	catJump(cat);
 }
 
 // adds to counter and randomly moves cat upon click
 const counter = document.querySelector(".counter");
-	for(let i = 0, len = cats.length; i < len; i++) {
-		cats[i].onclick = () => {
-		  meow.play();
-		  count += 1;
-			counter.innerHTML = "Clicks: " + count;
-			catJump(i);
-		};
-	}
+for(const cat of cats) {
+	cat.onclick = () => {
+	  meow.play();
+	  count += 1;
+		counter.innerHTML = "Clicks: " + count;
+		catJump(cat);
+	};
+}
 
 // resets counter upon click
 counter.onclick = () => {
 	count = 0;
-  counter.innerHTML = "Clicks: " + count;
+  	counter.innerHTML = "Clicks: " + count;
 };
 
 // resizes and moves cat upon window resize
@@ -62,8 +62,8 @@ window.addEventListener("resize", () => {
 	width = window.innerWidth;
 	height = window.innerHeight;
 	dietPlan();
-	for(let i = 0, len = cats.length; i < len; i++) {
-		catJump(i);
+	for(const cat of cats) {
+		catJump(cat);
 	}
 });
 
